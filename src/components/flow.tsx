f0fd5bb8a9acaad8650b8d4d59f8c65689ef47e4
EdamAme-x <edame8080@gmail.com>
2024-07-16 13:35:33 +0900

    add

--- src/components/flow.tsx
+++ src/components/flow.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
-import { obfuscate } from '../lib/obfuscate';
+import { obfuscate, inject } from '../lib/obfuscate';
 
 export function Flow() {
   const [pure, setPure] = useState(`console.log("Hello world");`);
-  const [parsed, setParsed] = useState(`Please press obfuscate button.`);
+  const [parsed, setParsed] = useState(`// Please press obfuscate button.`);
 
-  const parse = () => {
-    setParsed(obfuscate(pure));
+  const parse = async () => {
+    setParsed(await obfuscate(pure));
+  };
+
+  const injecter = () => {
+    setParsed(inject(parsed));
   };
 
   return (
@@ -19,11 +23,14 @@ export function Flow() {
         onInput={(e) => setPure((e.currentTarget as HTMLTextAreaElement).value)}
         defaultValue={pure}
       />
-      <Button variant="outline" className="my-2" onClick={parse}>
+      <Button variant="outline" className="my-2 mr-1" onClick={parse}>
         Obfuscate
       </Button>
+      <Button variant="outline" className="my-2" onClick={injecter}>
+        Inject Anti Debugger
+      </Button>
       <h2 className="font-mono">Obfuscated Code</h2>
-      <Textarea className="h-[30vh]" value={parsed} />
+      <Textarea className="h-[30vh]" value={parsed} readOnly />
     </div>
   );
 }
